4c59a32adb01409dbebd36c58c66a5b1a6b12f18
markzhitnitsky <mzhitnit@student.ubc.ca>
2025-09-12 15:38:43 +0200

    lots of refactoring to improve readability

--- ClassDefinitions.py
+++ ClassDefinitions.py
@@ -1,10 +1,17 @@
 import copy
 from typing import List
 import numpy as np
-from numpy.f2py.crackfortran import previous_context
 from numpy.typing import NDArray
 import requests
 from bs4 import BeautifulSoup
+
+WIKIHEADERS = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/120.0.0.0 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        }
 
 class NamedEntity:
     _instances = {}
@@ -71,27 +78,21 @@ class Quali:
 
     positions: List[QualiPosition]
 
-    def get_table_from_wiki_following_div(url: str) -> "Quali":
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/120.0.0.0 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        }
+    def get_table_from_wiki_following_div(url: str) -> List[QualiPosition]:
+
         # print(url)
         session = requests.Session()
-        resp = requests.get(url, headers=headers)
+        resp = requests.get(url, headers=WIKIHEADERS)
         resp.raise_for_status()
         soup = BeautifulSoup(resp.text, "html.parser")
 
         # Find the div containing the text prev_div
         prev_div_id = "Qualifying"
-        if ("1953_French" in url):
+        if "1953_French" in url:
             prev_div_id = "Qualifying_classification"
-        elif ("1950_Indianapolis" in url):
+        elif "1950_Indianapolis" in url:
             prev_div_id = prev_div_id
-        elif ("1957_Indianapolis" in url):
+        elif "1957_Indianapolis" in url:
             prev_div_id = "Grid"
         elif ("Indianapolis" in url):
             prev_div_id = "Starting_grid"
@@ -103,42 +104,21 @@ class Quali:
         except Exception as e:
             print("Table doesn't exist after div, e: ", e, "url: ", url)
             raise e
-        # table = div.find_next("table")
-        # except Exception as e:
-        #     print("Got error: ", e)
-        #     raise e
 
         rows = table.tbody.find_all("tr")
 
+        maxWidth, headerHeight = getHeaderDimensions(rows)
 
-        # rows = [r for r in all_rows if r.td]
-        # rows = all_rows
-        maxWidth = -1
-        headerHeight = 1
-        headerRow = rows[0]
-        header_cells = [cell for cell in headerRow if cell.name is not None]
-        for i, th in enumerate(header_cells):
-            if ("Gap" in th.name):
-                continue
-            if th.has_attr("colspan"):
-                maxWidth += int(th["colspan"])
-            else:
-                maxWidth += 1
-            if th.has_attr("rowspan"):
-                headerHeight = max(int(th["rowspan"]), headerHeight)
         # remove the header and footer
         rows = rows[headerHeight:-1]
-        # rows = rows[:-1]
 
-        # extract text for each cell
-        # dataTable = []
+        # extract relevant text from each cell
         qualiList: List[QualiPosition] = []
         for i, row in enumerate(rows):
             # row = [cell for cell in row if cell.name is not None]
             # if (row.get("class") and "sortbottom" in row.get("class")):
             #     continue
 
-            dataRow = []
             cells = row.find_all("td")
 
             if (len(cells) > maxWidth):
@@ -190,53 +170,15 @@ class Quali:
                     case _:
                         # else, record for fun
                         times.append(cell.text)
-                # a_tag = cell.find("a", recursive=False)  # td > a
-                # if a_tag is None:
-                #     # fallback: look for <a> inside a span
-                #     span = cell.find("span")
-                #     if span:
-                #         a_tag = span.find("a")
-                #
-                # if a_tag:
-                #     text = a_tag.get_text(strip=True)
-                #     href = a_tag.get("href")
-                # else:
-                #     text = cell.get_text(strip=True)
-                #     href = None
-
-                # dataRow.append([text, href])
-            # if (len(dataTable) and len(dataRow) < len(dataTable[-1])):
-            #     print("prev row was longer (quali) i= ", i, "url: ", url)
-
-            # print(len(dataRow))
-
-            # TODO: instead of saving as NDArray, save directly to the type I need?
-            # that way I avoid problems of adding useless columns of null
-            # this woudl require refactoring, and creating different versions of this function
-            # that convert to the right type
-
-            # dataRow.append([cell])
-            # if (len(dataRow) > 0):
-            # dataTable.append(dataRow)
             qualiList.append(QualiPosition(Driver(racerName, racerURL), Constructor(constructorName, constructorURL), gridPosition, times))
 
-        # returnVal = np.array(dataTable)
         print("QualiList   created for url:", url)
         # # print(returnVal)
         # return returnVal
-        return qualiList
+        return Quali(qualiList)
 
     def get_quali_from_link(url: str):
-        # qualiTable: NDArray = Quali.get_table_from_wiki_following_div(url, "Qualifying")
-        # # print(qualiTable)
-        # # TODO properly create instances of QualiPosition from the row
-        # qualiList: List[QualiPosition] = []
-        # for i,row in enumerate(qualiTable):
-        #     qualiPosition = QualiPosition(Driver(row[1][0],row[1][1]), Constructor(row[2][0],row[2][1]), row[3][0], i)
-        #     # TODO: need to get time or times depending on how many rounds of quali, if there even is one
-        #     qualiList.append(qualiPosition)
-        # return Quali(qualiList)
-        return Quali.get_table_from_wiki_following_div(url)
+        return Quali(Quali.get_table_from_wiki_following_div(url))
         # return ClassDefinitions.Quali([]) # stub
 
 
@@ -246,23 +188,18 @@ class Results:
 
     positions: List[RacePosition]
 
-    def get_table_from_wiki_following_div(url: str, prev_div_id: str) -> NDArray:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/120.0.0.0 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        }
-        # print(url)
+    def get_table_from_wiki_following_div(url: str) -> List[RacePosition]:
+
         session = requests.Session()
-        resp = requests.get(url, headers=headers)
+        resp = requests.get(url, headers=WIKIHEADERS)
         resp.raise_for_status()
         soup = BeautifulSoup(resp.text, "html.parser")
 
         # Find the div containing the text prev_div
-        # try:
-        if ("Indianapolis" in url):
+        prev_div_id = "Race"
+        if ("1953_French" in url):
+            prev_div_id = "Race_classification"
+        elif ("Indianapolis" in url):
             prev_div_id = "Box_score"
 
         div = soup.find(id=prev_div_id)
@@ -270,79 +207,88 @@ class Results:
         try:
             table = div.parent.find_next_sibling("table")  # get the table after that div
         except Exception as e:
-            print("getting table (Restults) got an exception: ", e, "url: ", url)
+            print("Table doesn't exist after div, e: ", e, "url: ", url)
             raise e
 
-        all_rows = table.tbody.find_all("tr")
+        rows = table.tbody.find_all("tr")
 
-        rows = [r for r in all_rows if r.td]
+        maxWidth, headerHeight = getHeaderDimensions(rows)
 
-        # extract text for each cell
-        dataTable = []
+        # remove the header and footer
+        header = rows[0:headerHeight]
+        gridIndex = getGridFromHeader(header)
+        driverIndex = getDriverFromHeader(header)
+        constructorIndex = getConstructorFromHeader(header)
+        lapsIndex = getLapsFromHeader(header)
+        timeIndex = getTimeFromHeader(header)
+        pointsIndex = getPointsFromHeader(header)
+        rows = rows[headerHeight:-1]
+
+        # extract relevant text from each cell
+        resultsList: List[RacePosition] = []
         for i, row in enumerate(rows):
-            if (row.get("class") and "sortbottom" in row.get("class")):
-                continue
-
-            dataRow = []
             cells = row.find_all("td")
 
-            # print(len(cells))
-            if (len(cells) < 7):
-                currCells = cells
-                cells = rows[i - 1].find_all("td")
-                cells[1] = currCells[0]
+            if (len(cells) > maxWidth):
+                print("more cells than useful columns, len(cells): ", len(cells), "maxWidth: ", maxWidth)
+                cells = cells[:maxWidth]
 
+
+            if (len(cells) < maxWidth):
+                # if row is short
+                cells = copy.deepcopy(cells)
+                prevCells = rows[i - 1].find_all("td")
+                # find the previous row
+                newRow = copy.deepcopy(row)
+                for j, cell in enumerate(prevCells):
+                    if cell.has_attr("rowspan"):
+                        # find every element with rowspan
+                        rowspan = int(cell["rowspan"])
+                        newCell = copy.deepcopy(cell)
+                        newCell["rowspan"] = rowspan - 1
+                        cells.insert(j,newCell)
+                        newRow.insert(2*j+2,'\n')
+                        newRow.insert(2*j+3,newCell)
+                # cells = copy.deepcopy(cells) # necessary?
+                rows[i] = newRow
+
+            laps = 0
+            time = ""
+            points = 0
+            racerName = ""
+            racerURL = ""
+            constructorName = ""
+            constructorURL = ""
             for j, cell in enumerate(cells):
                 # Try direct child <a> first
-                a_tag = cell.find("a", recursive=False)  # td > a
-                if a_tag is None:
-                    # fallback: look for <a> inside a span
-                    span = cell.find("span")
-                    if span:
-                        a_tag = span.find("a")
+                # these indices differ for different websites
+                if (j == driverIndex):
+                    # racer name
+                    racerName = cell.find_all("a")[-1].get_text()
+                    racerURL = "https://en.wikipedia.org" + cell.find_all("a")[-1]["href"]
+                elif (j == constructorIndex):
+                    # constructor name
+                    constructorName = cell.find_all("a")[-1].get_text()
+                    constructorURL = "https://en.wikipedia.org" + cell.find_all("a")[-1]["href"]
+                elif (j == lapsIndex):
+                    laps = cell.text
+                elif (j == timeIndex):
+                    time = cell.text
+                elif (j == pointsIndex):
+                    if len(cell.find_all("b")) > 0:
+                        points = cell.find_all("b")[0].get_text()
+                    else:
+                        points = 0
 
-                if a_tag:
-                    text = a_tag.get_text(strip=True)
-                    href = a_tag.get("href")
-                else:
-                    text = cell.get_text(strip=True)
-                    href = None
+            resultsList.append(RacePosition(Driver(racerName, racerURL), Constructor(constructorName, constructorURL), laps, time, points))
 
-                dataRow.append([text, href])
-            if (len(dataTable) and len(dataRow) < len(dataTable[-1])):
-                print("prev row was longer (results) j= ", j, "url: ", url)
-
-            # print(len(dataRow))
-
-            # TODO: instead of saving as NDArray, save directly to the type I need?
-            # that way I avoid problems of adding useless columns of null
-            # this woudl require refactoring, and creating different versions of this function
-            # that convert to the right type
-
-            # dataRow.append([cell])
-            # if (len(dataRow) > 0):
-            dataTable.append(dataRow)
-        # print(len(dataTable))
-
-        # TODO: somehow getting different length rows
-        # print(len(dataRow))
-
-        # Stub to get only one row
-        # return dataTable
-        returnVal = np.array(dataTable)
-        print("Results table created for url:", url)
-        return returnVal
+        print("ResultsList created for url:", url)
+        # # print(returnVal)
+        # return returnVal
+        return Results(resultsList)
 
     def get_results_from_link(url: str):
-        # TODO get the whole table, each row becomes one RacePosition
-        raceTable: NDArray = Results.get_table_from_wiki_following_div(url, "Race")
-        # print(raceTable)
-        # TODO properly create instances of RacePosition from the row
-        raceList: List[RacePosition] = []
-        for i,row in enumerate(raceTable):
-            racePosition = RacePosition(Driver(row[1][0]), Constructor(row[2][0]), row[3][0], row[4][0], row[6][0])
-            raceList.append(racePosition)
-        return Results(raceList)
+        return Results(Results.get_table_from_wiki_following_div(url))
 
 class Race:
     def __init__(self, url: str, year: int, circuit: str, date, quali:Quali, results:Results):
@@ -371,24 +317,12 @@ class Season:
 
 
     def get_table_from_wiki_following_div(url: str, prev_div_id: str) -> NDArray:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                          "AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/120.0.0.0 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        }
-        # print(url)
         session = requests.Session()
-        resp = requests.get(url, headers=headers)
+        resp = requests.get(url, headers=WIKIHEADERS)
         resp.raise_for_status()
         soup = BeautifulSoup(resp.text, "html.parser")
 
-        # Find the div containing the text prev_div
-        # try:
-
         div = soup.find(id=prev_div_id)
-        # print("prev div id: ", prev_div_id, ", div: ", div)
         try:
             table = div.parent.find_next_sibling("table")  # get the table after that div
         except Exception as e:
@@ -408,7 +342,6 @@ class Season:
             dataRow = []
             cells = row.find_all("td")
 
-            # print(len(cells))
             if (len(cells) < 6):
                 currCells = cells
                 cells = rows[i - 1].find_all("td")
@@ -435,23 +368,7 @@ class Season:
             if (len(dataTable) and len(dataRow) < len(dataTable[-1])):
                 print("prev row was longer (season) j= ", j, "url: ", url)
 
-            # print(len(dataRow))
-
-            # TODO: instead of saving as NDArray, save directly to the type I need?
-            # that way I avoid problems of adding useless columns of null
-            # this woudl require refactoring, and creating different versions of this function
-            # that convert to the right type
-
-            # dataRow.append([cell])
-            # if (len(dataRow) > 0):
             dataTable.append(dataRow)
-        # print(len(dataTable))
-
-        # TODO: somehow getting different length rows
-        # print(len(dataRow))
-
-        # Stub to get only one row
-        # return dataTable
         returnVal = np.array(dataTable)
         return returnVal
 
@@ -474,3 +391,47 @@ class Season:
         season_race_links = [row[-1][1] for row in season_table]
         season_races = np.char.add("https://en.wikipedia.org", season_race_links)
         return Season(url, Season.get_races_from_links(season_races))
+
+
+def getGridFromHeader(header) ->int:
+    return getIndexFromHeader(header, "Grid")
+def getDriverFromHeader(header) ->int:
+    return getIndexFromHeader(header, "Driver")
+def getConstructorFromHeader(header) ->int:
+    return getIndexFromHeader(header, "Constructor")
+def getLapsFromHeader(header) ->int:
+    return getIndexFromHeader(header, "Laps")
+def getTimeFromHeader(header) ->int:
+    return getIndexFromHeader(header, "Time")
+def getPointsFromHeader(header) ->int:
+    return getIndexFromHeader(header, "Points")
+
+def getIndexFromHeader(header, str):
+    if (len(header) == 1):
+        # simple, just go through, and get the one with the same text
+        for i, col, in enumerate(header):
+            if col.text == str:
+                return i
+    else:
+        # len header > 1
+        # relevant stuff is prolly in the first row anyways, just go through that
+        for i, col, in enumerate(header):
+            if col.text == str:
+                return i
+
+
+def getHeaderDimensions(rows) -> {int, int}:
+    maxWidth = -1
+    headerHeight = 1
+    headerRow = rows[0]
+    header_cells = [cell for cell in headerRow if cell.name is not None]
+    for i, th in enumerate(header_cells):
+        if ("Gap" in th.name):
+            continue
+        if th.has_attr("colspan"):
+            maxWidth += int(th["colspan"])
+        else:
+            maxWidth += 1
+        if th.has_attr("rowspan"):
+            headerHeight = max(int(th["rowspan"]), headerHeight)
+    return {headerHeight, maxWidth}
